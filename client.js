33bc4c26ce56e31d447c844e18b1bccffe945772
Kara Bayse <karabayse@gmail.com>
2018-08-15 15:09:57 -0500

    added global variables and DOM elements

---
+++ client.js
@@ -0,0 +1,13 @@
+// Global Variables
+let time = 5;
+let score = 0;
+// Initialize -> game is going on = true
+let isPlaying;
+
+// DOM Elements
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
