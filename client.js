05c6477c7bd0b15c24f1365e6981f8150c701a1f
Kara Bayse <karabayse@gmail.com>
2018-08-17 14:28:17 -0500

    added function to initialize the game

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+window.addEventListener('load', init);
+
 // Global Variables
 let time = 5;
 let score = 0;
@@ -39,3 +41,18 @@ const words = [
   'space',
   'definition'
 ];
+
+// Initialize Game
+function init() {
+  console.log('init');
+  // Load word from array
+  showWord(words);
+}
+
+// Pick and show random word
+// function showWord(words) {
+//   // Generate random array index
+//   const randIndex = Math.floor(Math.random() * words.length);
+//   // Output random word
+//   currentWord.
+// }
